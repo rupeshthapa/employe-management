6a1b77c39541af2702399f5526c3e7d98c8fd96f
ProfitXerp <leleventuresadv@gmail.com>
2025-05-18 12:02:55 +0545

    dashboard with conrtoller

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,1 @@
+<h1>This is a dashboard page.</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
